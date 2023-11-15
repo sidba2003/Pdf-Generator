4782c5a0fab1d30975254d6ebfc7187be0fed4c4
sidme <bahlsiddharth123@gmail.com>
2023-11-15 23:25:10 +0000

    Adding the code to manage the text font

--- FontSetter.py
+++ FontSetter.py
@@ -17,21 +17,20 @@ class FontSetter:
                          ".normal": lambda: self.setTextFormat(self.style, 12, self.indent, self.alignment),
                          ".italics": lambda: self.setTextFormat("Italics", self.font, self.indent, self.alignment),
                          ".regular": lambda: self.setTextFormat("Normal", self.font, self.indent, self.alignment),
-                         ".indent": lambda: self.setTextFormat(self.style, 12, self.indent, self.alignment),
-                         ".fill": lambda: self.setTextFormat(self.style, 12, self.indent, TA_JUSTIFY),
-                         ".nofill": lambda: self.setTextFormat(self.style, 12, self.indent, TA_LEFT),
+                         ".indent": lambda: self.setTextFormat(self.style, self.font, self.indent, self.alignment),
+                         ".fill": lambda: self.setTextFormat(self.style, self.font, self.indent, TA_JUSTIFY),
+                         ".nofill": lambda: self.setTextFormat(self.style, self.font, self.indent, TA_LEFT),
                          ".bold": lambda: self.setTextFormat("Bold", self.font, self.indent, self.alignment)}
 
-    def setStyle(self, command: str, indent=0, alignment=TA_LEFT):
+    def setStyle(self, command: str, indent=0):
         if command in self.commands:
-            self.indent = self.indent + indent
-            self.alignment = alignment
+            self.indent = self.indent + (indent * 10)
             return self.commands[command]()
         raise ValueError("Please check the command. The provided command is", command)
 
     def setTextFormat(self, style, font, indent, alignment):
         # updating the text style values
-        self.style, self.font = style, font
+        self.style, self.font, self.alignment = style, font, alignment
 
         custom_style = ParagraphStyle(
             'Custom_Style',
