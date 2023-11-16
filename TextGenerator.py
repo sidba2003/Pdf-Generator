9eaec32c31f20cbfa49bd4763ee20ccd90be4452
sidme <bahlsiddharth123@gmail.com>
2023-11-16 00:41:01 +0000

    Adding the code to manage the text font

--- TextGenerator.py
+++ TextGenerator.py
@@ -13,33 +13,36 @@ class TextGenerator:
 
         self.page_content = []
 
-        self.commands = {".large", ".normal", ".italics", ".regular", ".indent", ".fill", ".nofill", ".bold"}
-
         self.document = SimpleDocTemplate("output.pdf", pagesize=letter)
 
     def handleLine(self, line: str) -> None:
         if line[0] == ".":
-            self.__addParagraph()
-            command_values = line.split(" ")
-            cmd = command_values[0]
-            if cmd == ".paragraph":
-                self.page_content.append(Paragraph("&nbsp;", self.font_style))
-            elif cmd == ".indent":
-                if len(command_values) < 2:
-                    raise ValueError("Please enter the correct command along with its values")
-                self.font_style = self.text_setter.setStyle(command=cmd, indent=int(command_values[1]))
-            else:
-                self.font_style = self.text_setter.setStyle(command=cmd)
+            self.__useCommand(line)
         else:
             self.line.append(line)
 
+    def __useCommand(self, line: str) -> None:
+        self.__addParagraph()
+        command_values = line.split(" ")
+        cmd = command_values[0]
+        if cmd == ".paragraph":
+            self.page_content.append(Paragraph("&nbsp;", self.text_setter.setStyle(command=".normal")))
+        elif cmd == ".indent":
+            if len(command_values) < 2:
+                raise ValueError("Please enter the correct command along with its values")
+            self.font_style = self.text_setter.setStyle(command=cmd, indent=int(command_values[1]))
+        else:
+            self.font_style = self.text_setter.setStyle(command=cmd)
+
     def __addParagraph(self):
         if self.line:
-            print(self.line)
             paragraph = Paragraph(" ".join(self.line), self.font_style)
             self.page_content.append(paragraph)
             self.line.clear()
 
     def addContentsToDocument(self) -> None:
         self.__addParagraph()
+        for i in self.page_content:
+            print(i)
+            print()
         self.document.build(self.page_content)
